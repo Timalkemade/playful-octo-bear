90444cedebc1411e27d0f5057472f9128c0723be
TimAlkemade <timalkemade@gmail.com>
2013-01-26 16:24:35 +0100

    add getters for player repulsors and attractors

--- src/main/java/nl/sest/gamejam/model/impl/Model.java
+++ src/main/java/nl/sest/gamejam/model/impl/Model.java
@@ -6,7 +6,6 @@ import nl.sest.gamejam.model.event.listener.CreatePhysicalListener;
 import nl.sest.gamejam.model.event.listener.DeletePhysicalListener;
 import nl.sest.gamejam.model.player.PlayerAttractor;
 import nl.sest.gamejam.model.player.PlayerRepulsor;
-import org.jbox2d.dynamics.World;
 
 import java.util.*;
 
@@ -18,14 +17,14 @@ import java.util.*;
  */
 public class Model {
 
-    private final Stack<Collision> collisions = new Stack<Collision>();
-    private final Set<Bob> bobs = new HashSet<Bob>();
-    private final Set<PlayerAttractor> playerAttractors = new HashSet<PlayerAttractor>();
-    private final Set<PlayerRepulsor> playerRepulsors = new HashSet<PlayerRepulsor>();
+	private final Stack<Collision> collisions = new Stack<Collision>();
+	private final Set<Bob> bobs = new HashSet<Bob>();
+	private final Set<PlayerAttractor> playerAttractors = new HashSet<PlayerAttractor>();
+	private final Set<PlayerRepulsor> playerRepulsors = new HashSet<PlayerRepulsor>();
 
 	private final List<PointOfInterest> pointsOfInterest = new ArrayList<PointOfInterest>();
 
-    private final Set<Obstacle> obstacles = new HashSet<Obstacle>();
+	private final Set<Obstacle> obstacles = new HashSet<Obstacle>();
 
 	private final ArrayList<TrainDestination> trainDestinations = new ArrayList<TrainDestination>();
 
@@ -83,18 +82,20 @@ public class Model {
 		bobs.add(bob);
 		fireCreateListeners(bob);
 	}
+
 	public void removeBob(Bob bob) {
 		bobs.remove(bob);
 		fireDeleteListeners(bob);
 	}
-    
-    /**
-     * Get all the Bobs in the world
-     * @return
-     */
-    public Set<Bob> getBobs() {
-    	return bobs;
-    }
+
+	/**
+	 * Get all the Bobs in the world
+	 *
+	 * @return
+	 */
+	public Set<Bob> getBobs() {
+		return bobs;
+	}
 
 	public ArrayList<TrainDestination> getTrainDestinations() {
 		return trainDestinations;
@@ -103,23 +104,25 @@ public class Model {
 	public void addPointOfInterest(PointOfInterest pointOfInterest) {
 		pointsOfInterest.add(pointOfInterest);
 	}
-    
-    /**
-     * Add an Obstacle to the Model
-     * @param o
-     */
-    public void addObstacle(Obstacle o) {
-    	obstacles.add(o);
-    	fireCreateListeners(o);
-    }
-    
-    /**
-     * Get all Obstacles from the Model
-     * @return
-     */
-    public Set<Obstacle> getObstacles() {
-    	return obstacles;
-    }
+
+	/**
+	 * Add an Obstacle to the Model
+	 *
+	 * @param o
+	 */
+	public void addObstacle(Obstacle o) {
+		obstacles.add(o);
+		fireCreateListeners(o);
+	}
+
+	/**
+	 * Get all Obstacles from the Model
+	 *
+	 * @return
+	 */
+	public Set<Obstacle> getObstacles() {
+		return obstacles;
+	}
 
 	public void removePointOfInterest(PointOfInterest pointOfInterest) {
 		pointsOfInterest.remove(pointOfInterest);
@@ -133,12 +136,21 @@ public class Model {
 		playerAttractors.remove(playerAttractor);
 	}
 
+	public Collection<PlayerAttractor> getPlayerAttractors() {
+		return Collections.unmodifiableCollection(playerAttractors);
+	}
+
+
 	public void addPlayerRepulsor(PlayerRepulsor playerRepulsor) {
 		playerRepulsors.add(playerRepulsor);
 	}
 
 	public void removePlayerRepulsor(PlayerRepulsor playerRepulsor) {
 		playerRepulsors.remove(playerRepulsor);
+	}
+
+	public Collection<PlayerRepulsor> getPlayerRepulsors() {
+		return Collections.unmodifiableCollection(playerRepulsors);
 	}
 
 	/**
