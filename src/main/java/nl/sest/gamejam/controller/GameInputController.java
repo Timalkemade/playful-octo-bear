a66c73a3a7b7c732603e93d431dc52b94b62fdd6
TimAlkemade <timalkemade@gmail.com>
2013-01-26 16:29:01 +0100

    add repulsor on right click

--- src/main/java/nl/sest/gamejam/controller/GameInputController.java
+++ src/main/java/nl/sest/gamejam/controller/GameInputController.java
@@ -2,6 +2,7 @@ package nl.sest.gamejam.controller;
 
 import nl.sest.gamejam.model.impl.Model;
 import nl.sest.gamejam.model.player.PlayerAttractor;
+import nl.sest.gamejam.model.player.PlayerRepulsor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,5 +28,12 @@ public class GameInputController {
 
 		PlayerAttractor playerAttractor = new PlayerAttractor(x, y);
 		model.addPlayerAttractor(playerAttractor);
+	}
+
+	public void handleRightClick(float x, float y) {
+		LOGGER.debug("Right click at [{}, {}]", x, y);
+
+		PlayerRepulsor playerRepulsor = new PlayerRepulsor(x, y);
+		model.addPlayerRepulsor(playerRepulsor);
 	}
 }
