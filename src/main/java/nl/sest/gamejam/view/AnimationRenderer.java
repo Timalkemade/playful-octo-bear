b01748a13798d5c9ecaba833891db489ed8ea100
JMIEGHEM <j.gelon@gmail.com>
2013-01-27 11:17:20 +0100

    Animated attractor/repulsor

--- src/main/java/nl/sest/gamejam/view/AnimationRenderer.java
+++ src/main/java/nl/sest/gamejam/view/AnimationRenderer.java
@@ -14,7 +14,7 @@ import org.slf4j.LoggerFactory;
  */
 public class AnimationRenderer implements Renderer {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ImageRenderer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(AnimationRenderer.class);
 
     private AnimationRenderable object;
     private Animation animation;
