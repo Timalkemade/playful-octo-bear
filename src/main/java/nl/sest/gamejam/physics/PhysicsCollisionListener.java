b6824a137a3cc5e104d55ee59cbfa33a95934e85
Bobbie Smulders <mail@bsmulders.com>
2013-01-27 10:18:34 +0100

    PhysicalInterface now also fires PitCollisionEvents

--- src/main/java/nl/sest/gamejam/physics/PhysicsCollisionListener.java
+++ src/main/java/nl/sest/gamejam/physics/PhysicsCollisionListener.java
@@ -2,6 +2,7 @@ package nl.sest.gamejam.physics;
 
 import nl.sest.gamejam.model.impl.Bob;
 import nl.sest.gamejam.model.impl.Obstacle;
+import nl.sest.gamejam.model.impl.Pit;
 import nl.sest.gamejam.model.impl.Valuable;
 
 public interface PhysicsCollisionListener {
@@ -9,5 +10,7 @@ public interface PhysicsCollisionListener {
 	void obstacleCollisionEvent(Bob bob, Obstacle obstacle); 
 	
 	void valuableCollisionEvent(Bob bob, Valuable valuable);
+	
+	void pitCollisionEvent(Bob bob, Pit pit);
 	
 }
